b2bdf1b00ad44e725d1d8745e8945d6845ef36d9
wmswm <wmswm@211.222.232.230>
2022-05-08 20:48:40 +0900

    해결

--- src/com/javaex/ex07/Friend.java
+++ src/com/javaex/ex07/Friend.java
@@ -1,16 +1,12 @@
 package com.javaex.ex07;
 
 public class Friend {
-
+	//필드
 	private String name;
 	private String hp;
 	private String school;
 
 	// getter/setter 작성
-
-	public void showInfo() {
-		System.out.println("이름:" + name + "  핸드폰:" + hp + "  학교:" + school);
-	}
 
 	public String getName() {
 		return name;
@@ -34,6 +30,10 @@ public class Friend {
 
 	public void setSchool(String school) {
 		this.school = school;
+	}
+	//메소드 일반
+	public void showInfo() {
+		System.out.println("이름:" + name + "  핸드폰:" + hp + "  학교:" + school);
 	}
 
 }
--- src/com/javaex/ex07/FriendApp.java
+++ src/com/javaex/ex07/FriendApp.java
@@ -13,17 +13,20 @@ public class FriendApp {
 
 		// 친구정보 3명 입력 로직 --> 반복문 사용
 		for (int i = 0; i < 3; i++) {
+			friendArray[i] = new Friend();
 			System.out.print("이름:");
-			
+			friendArray[i].setName(sc.next());
 			System.out.print("핸드폰:");
-			
+			friendArray[i].setHp(sc.next());
 			System.out.print("학교:");
-			
+			friendArray[i].setSchool(sc.next());
+
 		}
 
 		// 친구정보 출력
 		for (int i = 0; i < friendArray.length; i++) {
 			// 친구정보 출력 메소드 호출
+			friendArray[i].showInfo();
 		}
 
 		sc.close();
